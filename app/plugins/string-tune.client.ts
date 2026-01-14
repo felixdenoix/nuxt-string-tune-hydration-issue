a301ac302f39a3556139c37b0ba69ab352a2580d
Felix Lafeuille <felix.denoix@gmail.com>
2026-01-14 12:31:35 +0100

    fix plugin init

--- app/plugins/string-tune.client.ts
+++ app/plugins/string-tune.client.ts
@@ -4,15 +4,34 @@ import StringTune, {
   StringProgress,
 } from "@fiddle-digital/string-tune";
 
-export default defineNuxtPlugin(() => {
-  const stringTune = StringTune.getInstance();
-  stringTune.use(StringParallax);
-  stringTune.use(StringCursor);
-  stringTune.use(StringProgress);
-  stringTune.start(60);
-  return {
-    provide: {
-      stringTune,
-    },
-  };
+export default defineNuxtPlugin({
+  name: "string-tune",
+  parallel: true,
+  setup: (nuxtApp) => {
+    const stringTune = StringTune.getInstance();
+    stringTune.use(StringParallax);
+    stringTune.use(StringCursor);
+    stringTune.use(StringProgress);
+
+    nuxtApp.hook("app:mounted", () => {
+      nextTick(() => {
+        // Helper to ensure hydration is finished
+        const safeStart = () => {
+          if (!nuxtApp.isHydrating) {
+            stringTune.start(60);
+          } else {
+            requestAnimationFrame(safeStart);
+          }
+        };
+
+        safeStart();
+      });
+    });
+
+    return {
+      provide: {
+        stringTune,
+      },
+    };
+  },
 });
