24b00937bd1a9cdd5bb2b11c4606763be7936c48
Felix Lafeuille <felix.denoix@gmail.com>
2026-01-14 12:44:55 +0100

    scope transition styles

--- app/app.vue
+++ app/app.vue
@@ -1,18 +1,32 @@
+<script lang="ts" setup>
+  const logSomeError = (e: unknown) => {
+    console.error(e);
+  }
+</script>
+
 <template>
+  <NuxtErrorBoundary @error="logSomeError">
   <main>
     <div class="test" v-for="i in 10" :key="i" string string-repeat>
       <p>Block {{ i }}</p>
       <img :src="`https://placeholdit.com/600x400/dddddd/999999?text_${i}`"></img>
     </div>
   </main>
+  </NuxtErrorBoundary>
 </template>
 
-<style>
+<style lang="postcss">
 .test {
+  /*// Default static styles*/
   border: 1px solid transparent;
-  transition: border 1s ease;
-}
-.test.-inview {
-  border: 1px solid red;
+
+  /*// Animation styles only when plugin is ready*/
+  html.-string & {
+    transition: border 1s ease;
+
+    &.-inview {
+      border: 1px solid red;
+    }
+  }
 }
 </style>
